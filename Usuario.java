bad65985a568247b618eb2ca0b2cf3c98a4c2e88
Francisco Aguilar <francisco274aguilar@gmail.com>
2020-04-04 20:22:50 -0300

    Agrego Usuario

---
+++ Usuario.java
@@ -0,0 +1,10 @@
+package Tarea;
+public class Usuario{
+
+    private String nombre;
+    private int cedula;
+    private String contrasena;
+    private int edad;
+    private int cantHijos;
+    private int saldo;
+}
