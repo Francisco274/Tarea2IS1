c29ef80535e965481757d76748755a2ece455f15
Francisco Aguilar <francisco274aguilar@gmail.com>
2020-04-04 20:23:41 -0300

    Agrego get, set y constructor

--- Usuario.java
+++ Usuario.java
@@ -7,4 +7,56 @@ public class Usuario{
     private int edad;
     private int cantHijos;
     private int saldo;
+
+    public Usuario(){
+        nombre = "John Doe";
+        cedula = 123;
+        contrasena = "johnnie123";
+        edad = 0;
+        cantHijos = 0;
+        saldo = 0;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public int getCedula(){
+        return cedula;
+    }
+    public void setCedula(int cedula){
+        this.cedula = cedula;
+    }
+    
+    public String getContrasena(){
+        return contrasena;
+    }
+    public void setContrasena(String contra){
+        contrasena = contra;
+    }
+    
+    public int getEdad(){
+        return edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    
+    public int getCantHijos(){
+        return cantHijos;
+    }
+    public void setCantHijos(int n){
+        cantHijos = n;
+    }
+    
+    public int getSaldo(){
+        return saldo;
+    }
+    public void setSaldo(int saldo){
+        this.saldo = saldo;
+    }
+
 }
